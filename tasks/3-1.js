6c777d1bd34bda981560fe43223336a85ae720a0
Samuel Larsson <samuel@familjenlarsson.eu>
2022-12-03 06:29:45 +0100

    Day 3

---
+++ tasks/3-1.js
@@ -0,0 +1,22 @@
+const { input } = process.env;
+
+let sum = 0;
+
+input.split('\n').forEach((line) => {
+	const comp1 = line.substring(0, line.length / 2);
+	const comp2 = line.substring(line.length / 2);
+	sum += priority(commonItem(comp1, comp2));
+});
+
+console.log(sum);
+
+function commonItem(comp1, comp2) {
+	for (const c in comp1.split('')) {
+		if (comp2.includes(comp1[c])) return comp1[c];
+	}
+}
+
+function priority(item) {
+	if (item.match(/[A-Z]/)) return item.charCodeAt(0) - 38;
+	return item.charCodeAt(0) - 96;
+}
---
+++ tasks/3-2.js
@@ -0,0 +1,21 @@
+const { input } = process.env;
+
+let sum = 0;
+
+const lines = input.split('\n');
+for (let i = 0; i < lines.length; i += 3) {
+	sum += priority(commonItem(lines[i], lines[i + 1], lines[i + 2]));
+}
+
+console.log(sum);
+
+function commonItem(elf1, elf2, elf3) {
+	for (const c in elf1.split('')) {
+		if (elf2.includes(elf1[c]) && elf3.includes(elf1[c])) return elf1[c];
+	}
+}
+
+function priority(item) {
+	if (item.match(/[A-Z]/)) return item.charCodeAt(0) - 38;
+	return item.charCodeAt(0) - 96;
+}
