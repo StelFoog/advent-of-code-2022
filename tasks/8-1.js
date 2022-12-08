aa5303cf9a7c3da5d4dc987cedc6dabbb995d811
Samuel Larsson <samuel@familjenlarsson.eu>
2022-12-08 16:33:57 +0100

    Day 8

---
+++ tasks/8-1.js
@@ -0,0 +1,24 @@
+const { input } = process.env;
+
+const grid = input.split('\n').map((line) => line.split('').map(Number));
+
+let visibleTrees = grid.length * 2 + (grid[0].length - 2) * 2;
+for (let y = 1; y < grid.length - 1; y++) {
+	for (let x = 1; x < grid[y].length - 1; x++) {
+		if (isVisible(x, y)) visibleTrees++;
+	}
+}
+
+console.log(visibleTrees);
+
+function isVisible(x, y) {
+	const h = grid[y][x];
+	if (Math.max(...grid[y].slice(0, x)) < h) return true;
+	if (Math.max(...grid[y].slice(x + 1)) < h) return true;
+
+	const col = grid.map((row) => row[x]);
+	if (Math.max(...col.slice(0, y)) < h) return true;
+	if (Math.max(...col.slice(y + 1)) < h) return true;
+
+	return false;
+}
---
+++ tasks/8-2.js
@@ -0,0 +1,37 @@
+const { input } = process.env;
+
+const grid = input.split('\n').map((line) => line.split('').map(Number));
+
+let highScore = -1;
+for (let y = 1; y < grid.length - 1; y++) {
+	for (let x = 1; x < grid[y].length - 1; x++) {
+		const score = scenicScore(x, y);
+		if (score > highScore) highScore = score;
+	}
+}
+
+console.log(highScore);
+
+function scenicScore(x, y) {
+	const h = grid[y][x];
+	let top, left, right, bottom;
+	for (top = 1; true; top++) {
+		if (grid[y - top][x] >= h) break;
+		if (y - top <= 0) break;
+	}
+	for (bottom = 1; true; bottom++) {
+		if (grid[y + bottom][x] >= h) break;
+		if (y + bottom >= grid.length - 1) break;
+	}
+
+	for (left = 1; true; left++) {
+		if (grid[y][x - left] >= h) break;
+		if (x - left <= 0) break;
+	}
+	for (right = 1; true; right++) {
+		if (grid[y][x + right] >= h) break;
+		if (x + right >= grid[y].length - 1) break;
+	}
+
+	return top * left * right * bottom;
+}
